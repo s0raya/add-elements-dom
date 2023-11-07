c9f0442a2f869c8f7a1c52ae5062daae56e690ae
s0raya <soraya.alcobet@gmail.com>
2023-11-07 23:22:00 +0100

    ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
- // Aquí tu código
+// Aquí tu código
+
+const lista = document.getElementById('lista');
+const boton = document.getElementById('agregar');
+
+boton.addEventListener('click', function() {
+    const elemento = prompt("¿Que quieres agregar?");
+
+    // UNA FORMA DE HACERLO
+    const elementoLista = document.createElement("li");
+    elementoLista.innerText = elemento;
+    lista.appendChild(elementoLista);
+
+    // OTRA FORMA DE HACERLO MAS SENCILLA
+    //lista.insertAdjacentHTML('afterbegin', `<li>${elemento}</li>`)
+});
+
+
